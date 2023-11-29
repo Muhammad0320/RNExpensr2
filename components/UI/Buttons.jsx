67d32473a196ed81f2a00dfe968c972e3d0bb3c4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 20:33:42 +0100

    chore: add custom button

---
+++ components/UI/Buttons.jsx
@@ -0,0 +1,52 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+
+function Buttons({ children, mode, style, onPress }) {
+  return (
+    <View style={style}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <View style={[styles.container, mode === "flat" && styles.flat]}>
+          <Text style={[styles.btnText, mode === "flat" && styles.btnFlat]}>
+            {" "}
+            {children}{" "}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default Buttons;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: GlobalStyles.colors.primary500,
+    borderRadius: 6,
+    overflow: "hidden",
+    paddingBlock: 4,
+    paddingHorizontal: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  flat: {
+    backgroundColor: "transparent",
+  },
+
+  btnText: {
+    fontSize: 12,
+    textAlign: "center",
+    color: "white",
+  },
+
+  btnFlat: {
+    color: GlobalStyles.colors.primary200,
+  },
+
+  pressed: {
+    backgroundColor: GlobalStyles.colors.primary100,
+  },
+});
