076fed4f33ee0df7258f3b2d01591089bf492d07
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 20:47:24 +0100

    chore: manage expense comp

--- screens/ManageExpense.jsx
+++ screens/ManageExpense.jsx
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import IconButtons from "../components/UI/IconButtons";
+import { GlobalStyles } from "../constants/styles";
+import Buttons from "../components/UI/Buttons";
 
 function ManageExpense({ navigation, route }) {
   const id = route.params?.id;
@@ -10,7 +13,56 @@ function ManageExpense({ navigation, route }) {
     navigation.setOptions({ title: isEdit ? "Edit Expense" : "Add Expense" });
   }, [isEdit, navigation]);
 
-  return <View></View>;
+  const handleDeleteExpense = () => {};
+
+  const handleConfirm = () => {};
+
+  const handleCancel = () => {};
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Buttons mode="flat" onPress={handleCancel}>
+          {" "}
+          cancel{" "}
+        </Buttons>
+
+        <Buttons onPress={handleConfirm}> {isEdit ? "Edit" : "Add"} </Buttons>
+      </View>
+
+      {isEdit && (
+        <View style={styles.iconContainer}>
+          <IconButtons
+            name="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={handleDeleteExpense}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.primary800,
+    padding: 24,
+  },
+
+  iconContainer: {
+    paddingTop: 8,
+    marginTop: 16,
+    borderTopColor: GlobalStyles.colors.primary500,
+    borderTopWidth: 2,
+    textAlign: "center",
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    columnGap: 50,
+  },
+});
