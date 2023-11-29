8a78e79666d889ca6bb4b32ad4e49133cae8dc4c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 15:41:43 +0100

    chore: add expense summary

--- components/expense/ExpenseSummary.jsx
+++ components/expense/ExpenseSummary.jsx
@@ -1,14 +1,37 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpenseSummary({ period, expenses }) {
   const totalExpensesPrice = expenses.reduce((acc, sum) => acc + sum.amount, 0);
 
   return (
-    <View>
-      <Text> {period} </Text>
-      <Text> ${totalExpensesPrice.toFixed(2)} </Text>
+    <View style={styles.container}>
+      <Text style={styles.period}> {period} </Text>
+      <Text style={styles.amount}> ${totalExpensesPrice.toFixed(2)} </Text>
     </View>
   );
 }
 
 export default ExpenseSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  period: {
+    fontSize: 13,
+    color: GlobalStyles.colors.primary400,
+  },
+
+  amount: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary500,
+  },
+});
