c47a5226e271e1fb4b0a9056d386c6151c881ef7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 04:53:20 +0100

    chore: create bottom navigation container

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllExpenses from "./screens/AllExpenses";
+import RecentExpense from "./screens/RecentExpense";
+
+const Stack = createNativeStackNavigator();
+
+const BottomTabs = createBottomTabNavigator();
+
+const BottomTabsNavigator = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen component={AllExpenses} name="AllExpenses" />
+      <BottomTabs.Screen component={RecentExpense} name="RecentExpenses" />
+    </BottomTabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
+
+      <NavigationContainer></NavigationContainer>
     </>
   );
 }
