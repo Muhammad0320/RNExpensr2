a088b4ec44471c0b1f2aca32f9204bf35d3b9a8d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 12:22:17 +0100

    chore: add expense summary

---
+++ components/expense/ExpenseSummary.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+function ExpenseSummary() {
+  return (
+    <View>
+      <View>
+        <Text> A paitr of shoes </Text>
+        <Text> </Text>
+      </View>
+      <Text> 129.99 </Text>
+    </View>
+  );
+}
+
+export default ExpenseSummary;
