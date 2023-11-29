362bcd607fbe14d9b8eb6436edc4b4595dc8a72c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-29 15:22:13 +0100

    chore: add expenseList data item

--- components/expense/ExpeneList.jsx
+++ components/expense/ExpeneList.jsx
@@ -1,7 +1,14 @@
 import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
-function ExpeneList({ items }) {
-  return <FlatList />;
+function ExpeneList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={({ item }) => <ExpenseItem {...item} />}
+      keyExtractor={(item) => item.id}
+    />
+  );
 }
 
 export default ExpeneList;
