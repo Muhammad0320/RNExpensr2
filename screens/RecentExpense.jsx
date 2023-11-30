56a3a48781fb6e404dab174b2494af4ed467b4ba
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-30 07:36:58 +0100

    chore: add recent expense

--- screens/RecentExpense.jsx
+++ screens/RecentExpense.jsx
@@ -1,8 +1,17 @@
-import { Text } from "react-native";
 import ExpenseOutput from "../components/expense/ExpenseOutput";
+import { useExpenseContext } from "../store/expenseContext";
+import { calcLast7days } from "../utils/date";
 
 function RecentExpense() {
-  return <ExpenseOutput expensePeriod="Last 7 days" />;
+  const { expenses } = useExpenseContext();
+
+  const recentExpenses = expenses.filter((expense) => {
+    const sevenDaysAgo = calcLast7days(new Date(), 7);
+
+    return expense.date >= sevenDaysAgo && expense.date <= Date.now();
+  });
+
+  return <ExpenseOutput expensePeriod="Last 7 days" expense={recentExpenses} />;
 }
 
 export default RecentExpense;
