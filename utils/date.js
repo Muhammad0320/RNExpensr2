a922635bab2276ac733d9b19b2cef588a3cea9cd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-30 07:27:40 +0100

    chore: add calclast7days

--- utils/date.js
+++ utils/date.js
@@ -1,3 +1,7 @@
 export const dateFormatter = (date) => {
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
+
+export const calcLast7days = (date, day) => {
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - day}`;
+};
