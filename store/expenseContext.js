4dd05ec084dfcaa8ecbb0e45a6e2b8b02c4ed229
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-30 06:52:44 +0100

    chore: created expense reducer

--- store/expenseContext.js
+++ store/expenseContext.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { DUMMY_DATA } from "../components/expense/ExpenseOutput";
 
 const ExpenseContext = createContext({
   addExpense: ({ date, description, amount }) => {},
@@ -7,3 +8,38 @@ const ExpenseContext = createContext({
 
   updateExpense: (id, { date, description, amount }) => {},
 });
+
+const expenseReducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      const id = new Date() + Math.random();
+
+      return [...state, { ...action.payload.data, id }];
+    case UPDATE:
+      const expenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+
+      const expenseItem = state[expenseIndex];
+
+      const updatedExpense = { ...expenseItem, ...action.payload.data };
+
+      const updatedState = [...state];
+
+      updatedState[expenseIndex] = updatedExpense;
+
+      return updatedState;
+
+    case DELETE:
+      return state.filter((expense) => expense.id !== action.payload.id);
+
+    default:
+      [...state];
+  }
+};
+
+const ExpensePrivider = ({ children }) => {
+  const [expenseState, dispatch] = useReducer(expenseReducer, DUMMY_DATA);
+
+  <ExpenseContext.Provider> {children} </ExpenseContext.Provider>;
+};
